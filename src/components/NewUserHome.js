1f99ddbef66d8a9b79f45b0c0bd551a06ea9810b
Marianne Kelly <marianne_kelly@hotmail.com>
2017-05-15 23:09:57 -0400

    buttons & colors

--- src/components/NewUserHome.js
+++ src/components/NewUserHome.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { observer } from 'mobx-react'
 import store from '../store'
+import logo from '../images/logo.png'
 
 @observer
 class NewUserHome extends Component {
   render () {
     if (!store.newUser) {
       return <div className='NewUserHome'>
-        <h1>Ingredereader</h1>
-        <div>IMAGE GOES HERE</div>
+        <h1 className='title'>Ingredereader</h1>
+        <img src={logo} alt='' />
         <p>A simple way to read an ingredient label by scanning the bar code</p>
-        <button>sign in</button>
-        <button>sign up</button>
-        <button>choose item to scan</button>
+        <div className='buttonBarn'>
+          <button className='short1'>sign in</button>
+          <button className='short2'>sign up</button>
+          <button className='long'>choose item to scan</button>
+        </div>
       </div>
     } else {
       return <Redirect to='/welcome' />
