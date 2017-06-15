8606485e787368c9780a28698ad07a1b772736b3
Marianne Kelly <marianne_kelly@hotmail.com>
2017-06-15 11:30:33 -0400

    edits

--- src/components/NewUserHome.js
+++ src/components/NewUserHome.js
@@ -11,10 +11,10 @@ class NewUserHome extends Component {
       return <div className='NewUserHome'>
         <h1 className='title'>Ingredereader</h1>
         <img src={logo} alt='' />
-        <p>A simple way to read an ingredient label by scanning the bar code</p>
+        <p className='intro'>A simple way to find an ingredient label by scanning the bar code</p>
         <div className='buttonBarn'>
           <button className='long' onClick={() => auth.signIn()}>sign in</button>
-          <button className='long'>sign up</button>
+          {/* <button className='long'>sign up</button> */}
           <button className='long'><Link to='/choose'>Choose Ingredient</Link></button>
         </div>
       </div>
